e6b1f62ec94067207e2d2aa947cf1df29c0a47be
Youcef Sebiat <youcefsebiat@gmail.com>
2018-09-12 11:46:52 +0200

    CNAM-348: Exposures and Drug Purchases start stats

---
+++ src/exploration/stats/drug_purchases.py
@@ -0,0 +1,57 @@
+from matplotlib.figure import Figure
+
+from src.exploration.core.cohort import Cohort
+from src.exploration.core.decorators import title
+from src.exploration.stats.time_distribution import plot_events_per_month_as_bars, \
+    plot_events_per_week_as_bars, plot_events_per_day_as_bars, \
+    plot_events_per_month_as_timeseries, plot_events_per_week_as_timeseries, \
+    plot_events_per_day_as_timeseries
+
+registry = []
+
+
+def register(f):
+    registry.append(f)
+    return f
+
+
+@register
+@title("Drug Purchases per month")
+def plot_drug_purchases_per_month_as_bars(figure: Figure, cohort: Cohort) -> Figure:
+    f = plot_events_per_month_as_bars(figure, cohort)
+    return f
+
+
+@register
+@title("Drug Purchases per month")
+def plot_drug_purchases_per_week_as_bars(figure: Figure, cohort: Cohort) -> Figure:
+    plot_events_per_week_as_bars(figure, cohort)
+    return figure
+
+
+@register
+@title("Drug Purchases per month")
+def plot_drug_purchases_per_day_as_bars(figure: Figure, cohort: Cohort) -> Figure:
+    plot_events_per_day_as_bars(figure, cohort)
+    return figure
+
+
+@register
+@title("Drug Purchases per month")
+def plot_drug_purchases_per_month_as_timeseries(figure: Figure, cohort: Cohort) -> Figure:
+    plot_events_per_month_as_timeseries(figure, cohort)
+    return figure
+
+
+@register
+@title("Drug Purchases per month")
+def plot_drug_purchases_per_week_as_timeseries(figure: Figure, cohort: Cohort) -> Figure:
+    plot_events_per_week_as_timeseries(figure, cohort)
+    return figure
+
+
+@register
+@title("Drug Purchases per month")
+def plot_drug_purchases_per_day_as_timeseries(figure: Figure, cohort: Cohort) -> Figure:
+    plot_events_per_day_as_timeseries(figure, cohort)
+    return figure
