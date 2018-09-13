6a8b5aecb217f831a00f782148f0d706ddb54c95
Youcef Sebiat <youcefsebiat@gmail.com>
2018-09-13 15:47:51 +0200

    CNAM-348: Add fractures specialzed stats

--- src/exploration/stats/time_distribution.py
+++ src/exploration/stats/time_distribution.py
@@ -7,46 +7,48 @@ from src.exploration.core.cohort import Cohort
 from src.exploration.core.decorators import xlabel, ylabel
 
 
-def _prepare_data(outcomes: Cohort) -> pd.DataFrame:
-    data = outcomes.events.groupBy("start").count().toPandas().sort_values("start")
-    data = data.set_index(pd.DatetimeIndex(data.start, tz="Europe/Paris", ambiguous=True,
+def _set_start_as_index(data: pd.DataFrame) -> pd.DataFrame:
+    return data.set_index(pd.DatetimeIndex(data.start, tz="Europe/Paris", ambiguous=True,
                                            name="index")).sort_index()
-    return data.reset_index()
 
 
-def _per_month(data: pd.DataFrame) -> pd.DataFrame:
-    return data.groupby([data["index"].dt.year, data["index"].dt.month]).sum()["count"]
+def _per_month(data: pd.Series) -> pd.Series:
+    return data.groupby([data.index.year, data.index.month]).sum()
 
 
-def _per_week(data: pd.DataFrame) -> pd.DataFrame:
-    return data.groupby([data["index"].dt.year, data["index"].dt.week]).sum()["count"]
+def _per_week(data: pd.Series) -> pd.Series:
+    return data.groupby([data.index.year, data.index.week]).sum()
 
 
-def _per_day(data: pd.DataFrame) -> pd.DataFrame:
-    return data.groupby(data["index"]).sum()["count"]
+def _per_day(data: pd.Series) -> pd.Series:
+    return data.groupby([data.index.year, data.index.week, data.index.day]).sum()
 
 
-def _prepare_data_2(cohort: Cohort, date_unit: str) -> pd.DataFrame:
+def _time_unit(data: pd.Series, time_unit: str) -> pd.Series:
+    if time_unit == "day":
+        return _per_day(data)
+    elif time_unit == "month":
+        return _per_month(data)
+    elif time_unit == "week":
+        return _per_week(data)
+    else:
+        raise ValueError("Wrong date unit {}. day, month, week only.".format(time_unit))
+
+
+def _prepare_data(cohort: Cohort, date_unit: str) -> pd.Series:
     data = cohort.events.groupBy("start").count().toPandas().sort_values("start")
     data = data.set_index(pd.DatetimeIndex(data.start, tz="Europe/Paris", ambiguous=True,
                                            name="index")).sort_index().reset_index()
-    if date_unit == "day":
-        return _per_day(data)
-    elif date_unit == "month":
-        return _per_month(data)
-    elif date_unit == "week":
-        return _per_week(data)
-    else:
-        raise ValueError("Wrong date unit {}. day, month, week only.".format(date_unit))
+    return _time_unit(data, date_unit)
 
 
-def _plot_bars(data: pd.DataFrame, ax: Axes) -> Axes:
+def _plot_bars(data: pd.Series, ax: Axes) -> Axes:
     ax.bar(range(len(data.index)), data.values, color=sns.xkcd_rgb["pumpkin orange"],
            tick_label=data.index)
     return ax
 
 
-def _plot_line(data: pd.DataFrame, ax: Axes) -> Axes:
+def _plot_line(data: pd.Series, ax: Axes) -> Axes:
     data.plot(kind="line", color=sns.xkcd_rgb["pumpkin orange"])
     return ax
 
@@ -54,7 +56,7 @@ def _plot_line(data: pd.DataFrame, ax: Axes) -> Axes:
 @xlabel("Month")
 @ylabel("Count")
 def plot_events_per_month_as_bars(figure: Figure, cohort: Cohort) -> Figure:
-    data = _prepare_data_2(cohort, "month")
+    data = _prepare_data(cohort, "month")
     ax = figure.gca()
     ax = _plot_bars(data, ax)
     for i, label in enumerate(ax.xaxis.get_ticklabels()):
@@ -68,7 +70,7 @@ def plot_events_per_month_as_bars(figure: Figure, cohort: Cohort) -> Figure:
 @xlabel("Week")
 @ylabel("Count")
 def plot_events_per_week_as_bars(figure: Figure, cohort: Cohort) -> Figure:
-    data = _prepare_data_2(cohort, "week")
+    data = _prepare_data(cohort, "week")
     ax = figure.gca()
     ax = _plot_bars(data, ax)
     for i, label in enumerate(ax.xaxis.get_ticklabels()):
@@ -82,7 +84,7 @@ def plot_events_per_week_as_bars(figure: Figure, cohort: Cohort) -> Figure:
 @xlabel("Day")
 @ylabel("Count")
 def plot_events_per_day_as_bars(figure: Figure, cohort: Cohort) -> Figure:
-    data = _prepare_data_2(cohort, "day")
+    data = _prepare_data(cohort, "day")
     ax = figure.gca()
     ax = _plot_bars(data, ax)
     return figure
@@ -91,7 +93,7 @@ def plot_events_per_day_as_bars(figure: Figure, cohort: Cohort) -> Figure:
 @xlabel("Month")
 @ylabel("Count")
 def plot_events_per_month_as_timeseries(figure: Figure, cohort: Cohort) -> Figure:
-    data = _prepare_data_2(cohort, "month")
+    data = _prepare_data(cohort, "month")
     ax = figure.gca()
     ax = _plot_line(data, ax)
     return figure
@@ -100,7 +102,7 @@ def plot_events_per_month_as_timeseries(figure: Figure, cohort: Cohort) -> Figur
 @xlabel("Week")
 @ylabel("Count")
 def plot_events_per_week_as_timeseries(figure: Figure, cohort: Cohort) -> Figure:
-    data = _prepare_data_2(cohort, "week")
+    data = _prepare_data(cohort, "week")
     ax = figure.gca()
     ax = _plot_line(data, ax)
     return figure
@@ -109,7 +111,7 @@ def plot_events_per_week_as_timeseries(figure: Figure, cohort: Cohort) -> Figure
 @xlabel("Day")
 @ylabel("Count")
 def plot_events_per_day_as_timeseries(figure: Figure, cohort: Cohort) -> Figure:
-    data = _prepare_data_2(cohort, "day")
+    data = _prepare_data(cohort, "day")
     ax = figure.gca()
     ax = _plot_line(data, ax)
     return figure
--- src/exploration/study/fall/fractures.py
+++ src/exploration/study/fall/fractures.py
@@ -1,20 +1,53 @@
-from src.exploration.core.cohort import Cohort
-from src.exploration.core.decorators import logged, xlabel, ylabel, title, \
-    ylabel_fontsize
-from matplotlib.figure import Figure
 import logging
+from functools import lru_cache
+
 import seaborn as sns
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+from pandas import DataFrame as pdDataFrame
+
+from src.exploration.core.cohort import Cohort
+from src.exploration.core.decorators import logged, title, xlabel, ylabel
+from src.exploration.stats.time_distribution import _plot_bars, _set_start_as_index, \
+    _time_unit
+
+registry = []
 
 
+def register(f):
+    registry.append(f)
+    return f
+
+
+@lru_cache(maxsize=32)
+def _admission_count(events) -> pdDataFrame:
+    return events.groupBy(["patientID", "start"]).count().toPandas().sort_values(
+        "start")
+
+
+def _admission_count_per_start(cohort: Cohort) -> pdDataFrame:
+    return _admission_count(cohort.events).groupby("start")[
+        "patientID"].count().reset_index()
+
+
+def _plot_admission_per_time_unit(cohort: Cohort, time_unit: str, ax) -> Axes:
+    data = _admission_count_per_start(cohort)
+    data = _set_start_as_index(data)
+    data = _time_unit(data.patientID, time_unit)
+    _plot_bars(data, ax)
+    return ax
+
+
+@register
 @logged(logging.INFO, "X-CNAM", "Saving plot for fractures by site")
-@xlabel("Nombre de fractures")
+@xlabel("Fractures count")
 @ylabel("Site")
-@title("Distribution de nombre de fractures par site")
-@ylabel_fontsize(8)
-def plot_fractures_by_site_distribution(figure: Figure, cohort: Cohort) -> Figure:
+@title("Distribution of fractures per site")
+def plot_fractures_by_site(figure: Figure, cohort: Cohort) -> Figure:
     axe = figure.gca()
-    df = cohort.events
-    fractures_site = df.groupBy("groupID").count().toPandas().sort_values("count", ascending=True)
+    fractures_site = (
+        cohort.events.groupBy("groupID").count().toPandas().sort_values("count",
+                                                                        ascending=True))
     axe.barh(y=range(len(fractures_site)), width=fractures_site["count"].values,
              tick_label=fractures_site.groupID.values,
              color=sns.xkcd_rgb["pumpkin orange"], )
@@ -22,4 +55,63 @@ def plot_fractures_by_site_distribution(figure: Figure, cohort: Cohort) -> Figur
     return figure
 
 
-registry = [plot_fractures_by_site_distribution, ]
+@register
+@logged(logging.INFO, "X-CNAM", "Saving plot of fractures per admission")
+@xlabel("Admissions count")
+@ylabel("Fractures count per admission")
+@title("Distribution of fractures per admission")
+def plot_fractures_count_per_admission(figure: Figure, cohort: Cohort) -> Figure:
+    ax = figure.gca()
+    data = _admission_count(cohort.events)
+    sns.countplot(x="count", data=data, ax=ax)
+    ax.grid(True, which="major", axis="y", linestyle='-')
+    return figure
+
+
+@register
+@logged(logging.INFO, "X-CNAM", "Number of admission for fractures per patient")
+@xlabel("Admissions count")
+@ylabel("Subjects count")
+@title("Number of admission for fractures per patient")
+def plot_admission_number_per_patient(figure: Figure, cohort: Cohort) -> Figure:
+    ax = figure.gca()
+    data = _admission_count(cohort.events).groupby("patientID").count()
+    sns.countplot(data=data, x="start", ax=ax)
+    ax.grid(True, which="major", axis="y")
+    return figure
+
+
+@register
+@logged(logging.INFO, "X-CNAM", "Number of admission for fractures per day")
+@xlabel("Day")
+@ylabel("Admission count")
+@title("Admission distribution per day")
+def plot_admission_per_day(figure: Figure, cohort: Cohort) -> Figure:
+    ax = figure.gca()
+    _plot_admission_per_time_unit(cohort, "day", ax)
+    ax.grid(True, which="major", axis="y", linestyle='-')
+    return figure
+
+
+@register
+@logged(logging.INFO, "X-CNAM", "Number of admission for fractures per week")
+@xlabel("Week")
+@ylabel("Admission count")
+@title("Admission distribution per week")
+def plot_admission_per_week(figure: Figure, cohort: Cohort) -> Figure:
+    ax = figure.gca()
+    _plot_admission_per_time_unit(cohort, "week", ax)
+    ax.grid(True, which="major", axis="y", linestyle='-')
+    return figure
+
+
+@register
+@logged(logging.INFO, "X-CNAM", "Number of admission for fractures per month")
+@xlabel("Month")
+@ylabel("Admission count")
+@title("Admission distribution per month")
+def plot_admission_per_month(figure: Figure, cohort: Cohort) -> Figure:
+    ax = figure.gca()
+    _plot_admission_per_time_unit(cohort, "month", ax)
+    ax.grid(True, which="major", axis="y", linestyle='-')
+    return figure
