591da6639e85a0c6f9e24a11ce8800217468b883
deepPhong <dinh.nguyen@polytechnique.edu>
2018-09-23 14:40:42 +0200

    Minor corrections for nice plot titles

--- src/exploration/stats/event_patient_distribution.py
+++ src/exploration/stats/event_patient_distribution.py
@@ -21,7 +21,7 @@ def _get_distinct(data, group_columns) -> pd.DataFrame:
 @register
 @xlabel("Events count")
 @ylabel("Subjects count")
-@title("distinct events count per patient")
+@title("Distinct events count per patient")
 def plot_unique_event_distribution_per_patient(figure: Figure, cohort: Cohort) -> Figure:
     group_columns = ["patientID", "value"]
     data = agg(cohort.events, frozenset(group_columns), "count")
@@ -34,7 +34,7 @@ def plot_unique_event_distribution_per_patient(figure: Figure, cohort: Cohort) -
 @register
 @xlabel("Subjects count")
 @ylabel("Events count")
-@title("distinct patients count per event")
+@title("Distinct patients count per event")
 def plot_patient_distribution_per_unique_event(figure: Figure, cohort: Cohort) -> Figure:
     group_columns = ["patientID", "value"]
     data = _get_distinct(cohort.events, group_columns)
