e589708c82a20b441a62790dbe30bc28030ee6ee
Nguyen Dinh-Phong <dinhphongng@gmail.com>
2018-11-16 15:17:08 +0100

    Cosmetic changes for title and plots

--- src/exploration/stats/event_patient_distribution.py
+++ src/exploration/stats/event_patient_distribution.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 
 from src.exploration.core.cohort import Cohort
@@ -19,25 +20,27 @@ def _get_distinct(data, group_columns) -> pd.DataFrame:
 
 
 @register
-@xlabel("Events count")
-@ylabel("Subjects count")
-@title("Distinct events count per patient")
+@xlabel("Number of distinct events")
+@ylabel("Number of subjects")
+@title("Number of distinct events per subject")
 def plot_unique_event_distribution_per_patient(figure: Figure, cohort: Cohort) -> Figure:
     group_columns = ["patientID", "value"]
     data = agg(cohort.events, frozenset(group_columns), "count")
     data = _get_distinct(data, group_columns)
     data = data.groupby("patientID").count().reset_index().groupby("value").count()
-    sns.barplot(x=data.index.values, y=data.patientID.values)
+    sns.barplot(x=data.index.values, y=data.patientID.values, color="salmon")
+    plt.xticks(rotation=90)
     return figure
 
 
 @register
-@xlabel("Subjects count")
-@ylabel("Events count")
-@title("Distinct patients count per event")
+@xlabel("Events names")
+@ylabel("Number of distinct patients")
+@title("Number of distinct patients count per event")
 def plot_patient_distribution_per_unique_event(figure: Figure, cohort: Cohort) -> Figure:
     group_columns = ["patientID", "value"]
     data = _get_distinct(cohort.events, group_columns)
     data = agg(data[group_columns], frozenset(["value"]), "count")
-    sns.barplot(x=data.value.values, y=data["count(1)"].values)
+    sns.barplot(x=data.value.values, y=data["count(1)"].values, color="salmon")
+    plt.xticks(rotation=90)
     return figure
